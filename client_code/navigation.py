32de0e0ca23f24f582241918d546193c31b043d1
Owen Campbell <owen.campbell@tanti.org.uk>
2020-05-08 12:32:47 +0100

    Add version info

--- client_code/navigation.py
+++ client_code/navigation.py
@@ -15,6 +15,8 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from anvil import Label, Link, get_open_form, set_url_hash
 
+__version__ = "0.1.0"
+
 # A dict mapping a form's name to a further dict with the form's class and title
 _forms = {}
 
@@ -30,84 +32,81 @@ _title_label = Label()
 
 
 class register:
-  """A decorator to register a form in the _forms dict."""
-  def __init__(self, name, title=None):
-    self.name = name
-    self.title = title
-    
-  def __call__(self, cls):
-    _forms[self.name] = {
-      "class": cls,
-      "title": self.title
-    }
-    return cls
-  
-  
+    """A decorator to register a form in the _forms dict."""
+
+    def __init__(self, name, title=None):
+        self.name = name
+        self.title = title
+
+    def __call__(self, cls):
+        _forms[self.name] = {"class": cls, "title": self.title}
+        return cls
+
+
 def get_form(name):
-  """Create an instance of a registered form."""
-  return _forms[name]["class"]()
+    """Create an instance of a registered form."""
+    return _forms[name]["class"]()
 
 
 def _open_tagged_form(form_name):
-  """Use classic routing to open a registered form"""
-  form = get_form(form_name)
-  _title_label.text = _forms[form_name]["title"]
-  get_open_form().content_panel.clear()
-  get_open_form().content_panel.add_component(form)
-  
-  
+    """Use classic routing to open a registered form"""
+    form = get_form(form_name)
+    _title_label.text = _forms[form_name]["title"]
+    get_open_form().content_panel.clear()
+    get_open_form().content_panel.add_component(form)
+
+
 def _default_link_click(**event_args):
-  """A handler for navigation link click events
+    """A handler for navigation link click events
   
   * Clears the role of all links registered in this module
   * Set the calling link's role to 'selected'
   * Calls the relevant action for classic or hash routing
   """
-  for l in _links:
-      l.role = ""
-  link = event_args["sender"]
-  link.role = "selected"
-  actions = {
-    "classic": _open_tagged_form,
-    "hash": set_url_hash
-  }
-  actions[link.tag.routing](link.tag.target)
-  
-  
+    for l in _links:
+        l.role = ""
+    link = event_args["sender"]
+    link.role = "selected"
+    actions = {"classic": _open_tagged_form, "hash": set_url_hash}
+    actions[link.tag.routing](link.tag.target)
+
+
 def _visibility_event_handler(**event_args):
-  """A handler for all registered visibility events"""
-  items = _visibility[event_args["event_name"]]
-  for item in items:
-    item["link"].visible = item["visible"]
+    """A handler for all registered visibility events"""
+    items = _visibility[event_args["event_name"]]
+    for item in items:
+        item["link"].visible = item["visible"]
 
-    
+
 def _register_visibility(container, link, visibility):
-  """Register the visibility events and set the handler on the container"""
-  for event, visible in visibility.items():
-    if event not in _visibility:
-      _visibility[event] = []
-      container.set_event_handler(event, _visibility_event_handler)
-    _visibility[event].append({"link": link, "visible": bool(visible)})
+    """Register the visibility events and set the handler on the container"""
+    for event, visible in visibility.items():
+        if event not in _visibility:
+            _visibility[event] = []
+            container.set_event_handler(event, _visibility_event_handler)
+        _visibility[event].append({"link": link, "visible": bool(visible)})
 
-    
+
 def build_menu(container, items, with_title=True):
-  """Add links to the container and set their initial visibility"""
-  if with_title:
-    container.parent.add_component(_title_label, slot="title")
-  for item in items:
-    link = navigation_link(**item)
-    _links.append(link)
-    visibility = link.tag.visibility
-    if visibility is None:
-      link.visible = True
-    else:
-      link.visible = False
-      _register_visibility(container, link, visibility)
-    container.add_component(link)
-  
+    """Add links to the container and set their initial visibility"""
+    if with_title:
+        container.parent.add_component(_title_label, slot="title")
+    for item in items:
+        link = navigation_link(**item)
+        _links.append(link)
+        visibility = link.tag.visibility
+        if visibility is None:
+            link.visible = True
+        else:
+            link.visible = False
+            _register_visibility(container, link, visibility)
+        container.add_component(link)
 
-def navigation_link(text, routing="classic", target=None, icon=None, on_click=None, visibility=None):
-  """Create a link instance
+
+def navigation_link(
+    text, routing="classic", target=None, icon=None, on_click=None, visibility=None
+):
+    """Create a link instance
   
   Parameters
   ----------
@@ -124,14 +123,16 @@ def navigation_link(text, routing="classic", target=None, icon=None, on_click=No
     event handler to call when clicked
   visibility
   """
-  if routing not in ("classic", "hash"):
-    raise ValueError("A navigation link's routing must either be 'classic' or 'hash'")
-  link = Link(text=text, icon=icon)
-  link.tag.routing = routing
-  link.tag.target = target
-  link.tag.visibility = visibility
-  if on_click is None:
-    link.set_event_handler('click', _default_link_click)
-  else:
-    link.set_event_handler('click', on_click)
-  return link
+    if routing not in ("classic", "hash"):
+        raise ValueError(
+            "A navigation link's routing must either be 'classic' or 'hash'"
+        )
+    link = Link(text=text, icon=icon)
+    link.tag.routing = routing
+    link.tag.target = target
+    link.tag.visibility = visibility
+    if on_click is None:
+        link.set_event_handler("click", _default_link_click)
+    else:
+        link.set_event_handler("click", on_click)
+    return link
